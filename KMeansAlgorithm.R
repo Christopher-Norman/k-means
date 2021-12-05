cad1e7267e1c972cc5b2b5fe1168c4d683f24f1b
Christopher Norman <christopherjohnnorman@gmail.com>
2021-12-05 00:00:24 +0000

    Full implementation of simple k-means algorithm and analysis

--- KMeansAlgorithm.R
+++ KMeansAlgorithm.R
@@ -1,82 +1,97 @@
-KMeansAlgorithm = setRefClass("KMeansAlgorithm", fields = list(x = "matrix", k = "numeric", iter.max = "numeric", nstart = "numeric", groups = "list", group_allocation = "vector", cluster_means = "matrix"),
-                              methods = list(
-                                
-                                # Initialises and runs the K-Means algorithm
-                                start = function() {
-                                  groups <<- vector(mode = "list", length = k)
-                                  
-                                  # Initial assignment probability into k groups is 1/k
-                                  group_allocation <<- sample(1:(k), nrow(x), replace=T)
-                                  
-                                  for(i in 1:(nrow(x))){  # Each data point index will be assigned to each group
-                                    groups[[group_allocation[i]]] <<- c(groups[[group_allocation[i]]], i)
-                                  }
-                                  
-                                  for(iteration in 1:iter.max){
-                                    update()
-                                  }
-                                },
-                                
-                                # Updates the group assignments
-                                update = function() {
-                                  number_variables = dim(data)[2] # Number of variables for each data point
-                                  
-                                  groups <<- vector(mode = "list", length = k)
-                                  for(i in 1:(nrow(x))){  # Update sets of clusters with the ID's that are currently assigned to that cluster
-                                    groups[[group_allocation[i]]] <<- c(groups[[group_allocation[i]]], i)
-                                  }
-                                  
-                                  # First estimate group means
-                                  cluster_means <<- matrix(list(), nrow=k, ncol=number_variables)
-                                  for(i in 1:k){  # Append the values of all the points currently in each cluster
-                                    current_group_data = c()
-                                    for(j in groups[[i]]){
-                                      current_group_data = rbind(current_group_data, x[j,])
-                                    }
-                                    # Group mean estimators = (1/number of samples currently in the group)*(sum of variables)
-                                    cluster_means[i,] <<- (1/length(groups[[i]]))*colSums(current_group_data)
-                                  }
-                                  
-                                  for (i in 1:(nrow(x))){  # Check each data point for it's closest medioid
-                                    closest_group = NULL
-                                    closest_distance = 10000  # Arbitrary large value
-                                    for (group_number in 1:k) {
-                                      if(norm(x[i,] - unlist(cluster_means[group_number,]), type="2") < closest_distance){
-                                        closest_group = group_number
-                                        closest_distance = norm(x[i,] - unlist(cluster_means[group_number,]), type="2")
-                                      }
-                                    }
-                                    group_allocation[i] <<- closest_group  # Update group assignment
-                                  }
-                                },
-                                
-                                results = function(){
-                                  # Calculate within cluster and between cluster sum of squares
-                                  # Cluster means and sizes
-                                  
-                                  # Total within group sum of squares
-                                  tot.withinss = 0
-                                  for(group_number in 1:k){
-                                    for(i in groups[[group_number]]){
-                                      tot.withinss = tot.withinss + t((x[i,] - unlist(cluster_means[group_number,])))%*%(x[i,] - unlist(cluster_means[group_number,]))
-                                    }
-                                  }
-                                  print(tot.withinss)
-                                  
-                                  # Between group sum of squares
-                                  betweenss = 0 
-                                  global_mean = (1/nrow(x))*colSums(x)
-                                  
-                                  for(group_number in 1:k){
-                                    betweenss = betweenss + length(groups[[group_number]])%*%t(unlist(cluster_means[group_number,]) - global_mean)%*%(unlist(cluster_means[group_number,]) - global_mean)
-                                  }
-                                  print(nrow(x))
-                                  print(global_mean)
-                                  print(betweenss)
-                                  print("BETWEEN/ TOTAL SS =")
-                                  print(betweenss/tot.withinss)
-                                }
-                                )
-                            )
-
-
+KMeansAlgorithm = setRefClass("KMeansAlgorithm", fields = list(x = "matrix", k = "numeric", iter.max = "numeric", nstart = "numeric", groups = "list", group_allocation = "vector", cluster_means = "matrix", iter = "numeric", tot.withinss = "numeric", betweenss = "numeric", tot.ss = "numeric", single_point_initialisation = "logical"),
+  methods = list(
+      # Initialises and runs the K-Means algorithm
+      start = function() {
+        groups <<- vector(mode = "list", length = k)  #  Holds  the k current  groups  andthe  respective  indexes  assigned  to them
+        
+        if(single_point_initialisation == TRUE){  # Random single data point centres
+          number_variables = dim(x)[2] # Number of variables for each data point
+          cluster_means <<- matrix(list(), nrow=k, ncol=number_variables)
+          
+          for(i in 1:k){
+            cluster_means <<- x[sample(nrow(x),size=k,replace=FALSE),]
+          }
+          
+          for (i in 1:(nrow(x))){  # Check each data point for it's closest cluster mean
+            closest_group = NULL
+            closest_distance = 10000000  # Arbitrary large value
+            for (group_number in 1:k) {
+              if(norm(x[i,] - unlist(cluster_means[group_number,]), type="2") < closest_distance){
+                closest_group = group_number
+                closest_distance = norm(x[i,] - unlist(cluster_means[group_number,]), type="2")
+              }
+            }
+            group_allocation[i] <<- closest_group  # Update group assignment
+          }
+          
+        } else {  # Randomly assign each data point to k groups 
+          group_allocation <<- sample(1:(k), nrow(x), replace=T)
+        
+          for(i in 1:(nrow(x))){  # Each data point index will be assigned to each group
+            groups[[group_allocation[i]]] <<- c(groups[[group_allocation[i]]], i)
+          }
+        }
+        
+        iter <<- 0
+        for(iteration in 1:iter.max){  # Iterate through algorithm iter.max times
+          previous_group_allocation = group_allocation
+          update()
+          iter <<- iter + 1  # Increment iteration count by 1
+          if(identical(previous_group_allocation, group_allocation)){
+            break  # End loop when iteration doesn't change the group allocation
+          }
+        }
+        results()  # Get final variation calculations
+      },
+      
+      # Updates the group assignments
+      update = function() {
+        number_variables = dim(x)[2] # Number of variables for each data point
+        
+        groups <<- vector(mode = "list", length = k)
+        for(i in 1:(nrow(x))){  # Update sets of clusters with the ID's that are currently assigned to that cluster
+          groups[[group_allocation[i]]] <<- c(groups[[group_allocation[i]]], i)
+        }
+        
+        # First estimate group means
+        cluster_means <<- matrix(list(), nrow=k, ncol=number_variables)
+        for(i in 1:k){  # Append the values of all the points currently in each cluster
+          current_group_data = c()
+          for(j in groups[[i]]){
+            current_group_data = rbind(current_group_data, x[j,])
+          }
+          # Group mean estimators = (1/number of samples currently in the group)*(sum of variables)
+          cluster_means[i,] <<- (1/length(groups[[i]]))*colSums(current_group_data)
+        }
+        
+        for (i in 1:(nrow(x))){  # Check each data point for it's closest cluster mean
+          closest_group = NULL
+          closest_distance = 10000000  # Arbitrary large value
+          for (group_number in 1:k) {
+            if(norm(x[i,] - unlist(cluster_means[group_number,]), type="2") < closest_distance){
+              closest_group = group_number
+              closest_distance = norm(x[i,] - unlist(cluster_means[group_number,]), type="2")
+            }
+          }
+          group_allocation[i] <<- closest_group  # Update group assignment
+        }
+      },
+      
+      results = function(){
+        # Calculates within cluster and between cluster sum of squares
+        # Total within group sum of squares
+        tot.withinss <<- 0
+        for(group_number in 1:k){
+          for(i in groups[[group_number]]){
+            tot.withinss <<- tot.withinss + as.numeric(t((x[i,] - unlist(cluster_means[group_number,])))%*%(x[i,] - unlist(cluster_means[group_number,])))
+          }
+        }
+        betweenss <<- 0  # Between group sum of squares
+        global_mean = (1/nrow(x))*colSums(x)
+        for(group_number in 1:k){
+          betweenss <<- betweenss + as.numeric(length(groups[[group_number]])%*%t(unlist(cluster_means[group_number,]) - global_mean)%*%(unlist(cluster_means[group_number,]) - global_mean))
+        }
+        tot.ss <<- betweenss + tot.withinss
+      }
+      )
+  )
--- KMeansAnalysis.R
+++ KMeansAnalysis.R
@@ -1,47 +1,117 @@
 source("k-means/KMeansAlgorithm.R")
 library("mnormt") # R package for multivariate normal distribution
 
-# CREATE SYNTHETIC DATA
-set.seed(105)  # Set seed for reproducible results
+set.seed(158)  # Set seed for reproducible results
+# Data 1 4 multivariate normal distributions with equal variance
+mu1 = c(50, 10)
+mu2 = c(30, 90)
+mu3 = c(15, 40)
+mu4 = c(80, 40)
+sigma = 50*diag(2)
 
-# 4 multivariate normal distributions with equal variance
-mu1 = c(10, 10)
-mu2 = c(30, 10)
-mu3 = c(25, 40)
-mu4 = c(25, 25)
-sigma = 16*diag(2)
+group1 = rmnorm(n = 50, mu1, sigma)
+group2 = rmnorm(n = 50, mu2, sigma)
+group3 = rmnorm(n = 50, mu3, sigma)
+group4 = rmnorm(n = 50, mu4, sigma)
 
-group1 = rmnorm(n = 100, mu1, sigma)
-group2 = rmnorm(n = 100, mu2, sigma)
-group3 = rmnorm(n = 100, mu3, sigma)
-group4 = rmnorm(n = 100, mu4, sigma)
-
-plot(group1, xlim=range(0,60), ylim=range(0,60), main="Simulated Data", xlab="X1", ylab="X2", col="red")
+plot(group1, xlim=range(-20,120), ylim=range(-20,120), main="Simulated Data Set 1", xlab="X1", ylab="X2", col="red")
 points(group2, col="blue")
-points(group3, col="yellow")
+points(group3, col="orange")
 points(group4, col="green")
-
+legend("topright", title = "Cluster Colours",legend= c("Group A", "Group B", "Group C", "Group D")
+       ,fill = c("red", "blue", "orange", "green"), cex = 0.6)
 data = rbind(group1, group2, group3, group4)
 
+
 # Create K-Means object from implementation in KMeansAlgorithm.R
-km = KMeansAlgorithm(x = data, k = 4, iter.max = 20, nstart = 3)  
+km = KMeansAlgorithm(x = data, k = 4, iter.max = 20, nstart = 3,single_point_initialisation = FALSE)  
 km$start()  # Initalise algorithm
 
-test_data = c()
-for (index in 1:400){
-  if(km$group_allocation[index] == 3){
-    test_data = rbind(test_data, data[index,])
-  }
-}
-points(test_data[,1], test_data[,2])
+cluster_allocation = rep(c("a","b","c","d"),each=50)
 
+print("OUR IMPLEMENTAION DATA 1")
+cat("between_SS / total_SS = ", (km$betweenss/(km$tot.withinss + km$betweenss)*100), "%")
+km$cluster_means
+print("Cluster assignment matrix:")
+table(cluster_allocation, km$group_allocation)
+cat("Number of iterations:", km$iter)
+
+
+print("R IMPLEMENTAION DATA 1")
 km.r <- kmeans(data, 4, iter.max = 20)  # K-Means R package to compare
-print(km.r)
-print(km)
+cat("between_SS / total_SS = ", (km.r$betweenss/(km.r$tot.withinss + km.r$betweenss)*100), "%")
+km.r$centers
+print("Cluster assignment matrix:")
+table(cluster_allocation, km.r$cluster)
+cat("Number of iterations:", km.r$iter)
 
-print(km$cluster_means)
-print(km.r$centers)
 
-km$results()
-print(km.r$tot.withinss)
-print(km.r$betweenss)
+print("OUR IMPLEMENTAION DATA 1 SINGLE POINT CENTRES")
+km = KMeansAlgorithm(x = data, k = 4, iter.max = 20, nstart = 3,single_point_initialisation = TRUE)  
+km$start()  # Initalise algorithm
+
+cat("between_SS / total_SS = ", (km$betweenss/(km$tot.withinss + km$betweenss)*100), "%")
+km$cluster_means
+print("Cluster assignment matrix:")
+table(cluster_allocation, km$group_allocation)
+cat("Number of iterations:", km$iter)
+
+# Data 2
+mu1 = c(10, 10)
+mu2 = c(40, 10)
+mu3 = c(80, 80)
+mu4 = c(0, 90)
+sigma = 80*diag(2)
+
+group1 = rmnorm(n = 50, mu1, sigma)
+group2 = rmnorm(n = 50, mu2, sigma)
+group3 = rmnorm(n = 50, mu3, sigma)
+group4 = rmnorm(n = 50, mu4, sigma)
+
+plot(group1, xlim=range(-20,120), ylim=range(-20,120), main="Simulated Data Set 2", xlab="X1", ylab="X2", col="red")
+points(group2, col="blue")
+points(group3, col="orange")
+points(group4, col="green")
+legend("topright", title = "Cluster Colours",legend= c("Group A", "Group B", "Group C", "Group D")
+       ,fill = c("red", "blue", "orange", "green"), cex = 0.6)
+data_2 = rbind(group1, group2, group3, group4)
+
+try({  # Catch the error thrown when a cluster is empty
+# Create K-Means object from implementation in KMeansAlgorithm.R
+km = KMeansAlgorithm(x = data_2, k = 4, iter.max = 20, nstart = 3, single_point_initialisation = FALSE)  
+km$start()  # Initalise algorithm
+}, {print("Error, a cluster is empty")}, silent = FALSE)
+
+km = KMeansAlgorithm(x = data_2, k = 4, iter.max = 20, nstart = 3, single_point_initialisation = TRUE)  
+km$start()  # Initalise algorithm
+
+print("OUR IMPLEMENTAION DATA 2")
+cat("between_SS / total_SS = ", (km$betweenss/(km$tot.ss)*100), "%")
+km$cluster_means
+print("Cluster assignment matrix:")
+table(cluster_allocation, km$group_allocation)
+cat("Number of iterations:", km$iter)
+
+
+print("R IMPLEMENTAION DATA 2")
+km.r <- kmeans(data, 4, iter.max = 20)  # K-Means R package to compare
+cat("between_SS / total_SS = ", (km.r$betweenss/(km.r$totss)*100), "%")
+km.r$centers
+print("Cluster assignment matrix:")
+table(cluster_allocation, km.r$cluster)
+cat("Number of iterations:", km.r$iter)
+
+colour_list = c("red", "blue", "orange", "green")
+# PLOT OUR RESULT
+plot(c(), xlim=range(-20,120), ylim=range(-20,120), main="Our K-Means Implementation Data Set 2", xlab="X1", ylab="X2", col="red")
+for (index in 1:200){
+  colour = colour_list[km$group_allocation[index]]
+  points(data_2[index,1], data_2[index, 2], col = colour)
+}
+
+# PLOT R RESULT
+plot(c(), xlim=range(-20,120), ylim=range(-20,120), main="R K-Means Implementation Data Set 2", xlab="X1", ylab="X2", col="red")
+for (index in 1:200){
+  colour = colour_list[km.r$cluster[index]]
+  points(data_2[index,1], data_2[index, 2], col = colour)
+}
